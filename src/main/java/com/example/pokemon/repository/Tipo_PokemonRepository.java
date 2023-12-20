d13a4bb5c0dca1e2963a6bc86a5e455cecedbae8
Neider123 <neiderjoansesc@ufps.edu.co>
2023-12-20 10:20:11 -0500

    agrego mas servicios

--- src/main/java/com/example/pokemon/repository/Tipo_PokemonRepository.java
+++ src/main/java/com/example/pokemon/repository/Tipo_PokemonRepository.java
@@ -1,9 +1,15 @@
 package com.example.pokemon.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.pokemon.entities.Tipo_Pokemon;
 
-public interface Tipo_PokemonRepository extends JpaRepository< Tipo_Pokemon , Integer> {
 
+public interface Tipo_PokemonRepository extends JpaRepository< Tipo_Pokemon , Integer> {
+	/**
+	public List<Tipo_Pokemon> findByTipo(String tipo);
+	**/
 }
